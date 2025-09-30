cb753d01bbd6d0030afaea8098d90ddb86d0f42a
bibi oyugbo <obehioyeoyugbo@gmail.com>
2025-09-30 23:37:43 +0100

    Rebuilt frontend, removed extra / on baseurl

--- client/apiConfig.ts
+++ client/apiConfig.ts
@@ -1,4 +1,4 @@
 // const BASE_URL = 'http://localhost:3200';
-const BASE_URL = 'https://birthday-reminder-app-xalg.onrender.com/';
+const BASE_URL = 'https://birthday-reminder-app-xalg.onrender.com';
 
 export default BASE_URL;
